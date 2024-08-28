2d218cdcf3843d53f0a579409a8e88034ff23710
Sebas Jimenez <a01754993@tec.mx>
2024-08-27 19:30:24 -0600

    Server

--- serverML.py
+++ serverML.py
@@ -1,14 +1,26 @@
-#Python Libraries 
-from flask import Flask, request, jsonify, render_template
+# Python Libraries
+import os
+
+import joblib
 import numpy as np
-from load import joblib
-import os 
+from flask import Flask, jsonify, render_template, request
 from werkzeug.utils import secure_filename
 
-#Load model
-dt = joblib.load('dt1_ml.joblib')
-#Create Flask App
-server = Flask(__name__)
+# Load model
+dt = joblib.load("dt1_ml.joblib")
+# Create Flask App
+server = Flask(_name_)
 
-#Define route
-@server.route('/predictjson', methods=['POST'])
+
+# Define route
+@server.route("/predictjson", methods=["POST"])
+def predictjson():
+    data = request.json
+    print(data)
+    inputData = np.array([data["pH"], data["sulphates"], data["alcohol"]])
+    result = dt.predict(inputData.reshape(1, -1))
+    return jsonify({"Prediction": str(result[0])})
+
+
+if _name_ == "_main_":
+    server.run(debug=False, host="0.0.0.0", port=8080)
